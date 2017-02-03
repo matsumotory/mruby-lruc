b4fc55cb0c2dd578aba8c52bca9f7f207f16082d
MATSUMOTO Ryosuke <matsumotory@pepabo.com>
2017-02-03 15:53:57 +0900

    Add test for LRUC#{[],[]=}

--- test/mrb_lruc.rb
+++ test/mrb_lruc.rb
@@ -8,6 +8,12 @@ assert("LRUC#set/get") do
   assert_equal("fuga", l.get("hoge"))
 end
 
+assert("LRUC#[]=/[]") do
+  l = LRUC.new 1024, 1024
+  l["hoge"] = "fuga"
+  assert_equal("fuga", l["hoge"])
+end
+
 assert("LRUC#delete") do
   l = LRUC.new 1024, 1024
   l.set "hoge", "fuga"
